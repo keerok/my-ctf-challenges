b05b4571135f1f2a050ef545411db2dda2aa684e
m4k4br0 <keerok@protonmail.com>
2018-12-30 03:59:26 -0200

    Source chall - code

---
+++ hackaflag2018/source/index.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_GET['source'])){
+    highlight_file(__FILE__);
+    exit;
+}
+
+
+class Bank {
+    const EXTERNAL_DIRECTORY = '/tmp/';
+    private $id;
+    private $perda = 0;
+    private $compra = 0;
+
+    public function __construct($input) {
+        $this->id = rand(1, 1000);
+
+        foreach ($input as $campo => $count) {
+            $this->$campo = $count++;
+        }
+    }
+
+    public function __destruct() {
+        file_put_contents(
+            self::EXTERNAL_DIRECTORY . $this->id,
+            var_export(get_object_vars($this), true)
+        );
+    }
+}
+
+$bank = new Bank($_GET);
+
+//flag = HACKAFLAG{c0D3_r3vi3w_1s_1mport4nt}
